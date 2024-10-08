9d5f8324af6b232eb2b2d357e9a7f3d23373571b
zainjhere <zain.javed@softvira.com>
2024-10-08 12:41:37 +0500

    Finish up sidebars

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 // import localFont from "next/font/local";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
 
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
@@ -26,13 +30,22 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body className={`antialiased`}>
+          <Topbar />
+
+          <main className="flex flex-row">
+            <LeftSidebar />
+
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
